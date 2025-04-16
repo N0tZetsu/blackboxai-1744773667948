beff85426232fdee8342f9bfa389b703f78cd533
N0tZetsu <emamainn@gmail.com>
2025-04-16 00:21:13 -0300

    Add fivem-loadingscreen/fxmanifest.lua 1744773673822

---
+++ fivem-loadingscreen/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BlackboxAI'
+description 'Custom Loading Screen with Music, Video and Image Support'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'config.json'
+}
